fa16fecfbe84714ed2869f63d10d0650edf2d19c
Andrej Zachar <andrej@chocolatejar.eu>
2021-10-29 11:05:22 +0200

    feat(diff): row failure visualization

--- chispa/table_diff.py
+++ chispa/table_diff.py
@@ -1,5 +1,5 @@
 import difflib
-from wasabi import color
+from wasabi import color, diff_strings
 from chispa.bcolors import *
 
 
@@ -14,10 +14,13 @@ class TableDiff:
         df1_as_text = self._getShowString(df1)
         df2_as_text = self._getShowString(df2)
         raise DataFramesNotEqualError(
-            "\n" + bcolors.NC +
-            "\n** Actual **\n" + df1_as_text +
-            "\n** Expected **\n" + df2_as_text +
-            "\n** Diff **\n" + self.diff_strings(df1_as_text, df2_as_text) +
+            bcolors.NC + "\n\n" +
+            color("** Actual **", fg=16, bg="red") + "\n" +
+            df1_as_text + "\n" +
+            color("** Expected **", fg=16, bg="green") + "\n" +
+            df2_as_text +
+            "\n** Diff **\n" + 
+            self.diff_strings(df1_as_text, df2_as_text) +
             "\n** Comparison **\n" + t.get_string())
 
     def _getShowString(self, df, n=20, truncate=True, vertical=False):
@@ -27,16 +30,4 @@ class TableDiff:
             return(df._jdf.showString(n, int(truncate), vertical))
 
     def diff_strings(self, a, b):
-        output = []
-        matcher = difflib.SequenceMatcher(None, a, b)
-        for opcode, a0, a1, b0, b1 in matcher.get_opcodes():
-            if opcode == "equal":
-                output.append(a[a0:a1])
-            elif opcode == "insert":
-                output.append(color(b[b0:b1], fg=16, bg="green"))
-            elif opcode == "delete":
-                output.append(color(a[a0:a1], fg=16, bg="red"))
-            elif opcode == "replace":
-                output.append(color(b[b0:b1], fg=16, bg="green"))
-                output.append(color(a[a0:a1], fg=16, bg="red"))
-        return "".join(output)
+        return diff_strings(a, b)
